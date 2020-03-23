4bacfbc17eedab298e7075fef9b838734b678514
Alexander Polishchuk <apolishchuk52@gmail.com>
2020-03-23 12:57:10 +0300

    Added setuptools

---
+++ setup.py
@@ -0,0 +1,44 @@
+import setuptools
+
+import obm
+
+
+def read(file_name):
+    with open(file_name) as f:
+        content = f.read()
+    return content
+
+
+setuptools.setup(
+    name='obm',
+    version=obm.__version__,
+    packages=setuptools.find_packages(exclude=['tests*']),
+    install_requires=[],
+    extras_require={
+        'dev': [
+            'sphinx>=2.4,<3',
+            'sphinx-rtd-theme',
+            'pytest',
+            'pylint',
+            'mypy',
+            'rope',
+        ],
+    },
+    license='GNU Lesser General Public License v3 or later (LGPLv3+)',
+    description='Async blockchain node interacting tool with ORM-like api.',
+    long_description=read('README.rst'),
+    long_description_content_type='text/x-rst',
+    url='https://github.com/madnesspie/obm',
+    author='Alexander Polishchuk',
+    author_email='apolishchuk52@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+)
