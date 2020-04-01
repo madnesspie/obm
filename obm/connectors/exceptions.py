1233db0a7a6d66a767a94c6044d54a98828e27ca
Alexander Polishchuk <apolishchuk52@gmail.com>
2020-04-01 12:58:06 +0300

    Fixed copyright typo

--- obm/connectors/exceptions.py
+++ obm/connectors/exceptions.py
@@ -1,4 +1,4 @@
-# Copyright 2019-2020 Alexander Polishchuk
+# Copyright 2020 Alexander Polishchuk
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
